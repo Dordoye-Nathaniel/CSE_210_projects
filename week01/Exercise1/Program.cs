364de1caea36232d49d2ed9d53a9342c37c99ee6
Klenam <ndordoye@gmaail.com>
2026-05-09 07:51:26 +0000

    Added a code to display user name.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Program to print user name!");
+
+        // Ask user for surname and firstname separately
+        Console.WriteLine("Enter your surname: ");
+        string SurName = Console. ReadLine();
+        Console.WriteLine("Enter your firstname: ");
+        string FirstName = Console. ReadLine();
+
+        // concatenate firstname and surname
+        string name = FirstName + " " + SurName;
+
+        // Display name
+        Console.WriteLine($"Your name is {SurName}, {name}");
+        
     }
+
 }
