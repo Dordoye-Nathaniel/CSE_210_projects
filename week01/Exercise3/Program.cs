0f18142ae1af840421cc8a20f020e801ab25d91e
Klenam <ndordoye@gmaail.com>
2026-05-09 07:53:18 +0000

    Added a magic number guess game.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,55 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    // Create an instance of the random class
+    static Random RandomGenerator = new Random();
+
+    static void Main()
     {
+        // Display a welcome message
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Call funciton to start the guess game
+        GuessCompareRandom();
     }
+
+    static int Generator()
+    {
+        // Store the random number generated in randonNumber variable
+        // Return the random number
+        int randomNumber = RandomGenerator.Next(1, 101);
+        return randomNumber;
+    }
+
+    static void GuessCompareRandom()
+    {
+        // Call the random generator method and store the number in random variable
+        int random = Generator();
+
+        // number of guess
+        int guessCount = 0;
+
+        // declare a flag to reset the while loop 
+        string response = "yes";
+        while (response == "yes")
+        {
+            Console.Write("Guess a magic number: ");
+            int guess = int.Parse(Console.ReadLine());  
+            guessCount++;
+
+            if(guess > random)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else if(guess < random)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed right in {guessCount} !");
+                response = "no";
+            }
+        }
+    }   
 }
