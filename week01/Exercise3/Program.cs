379f2e9d63ba6107294dab3ab1f96a56a2eb672f
Klenam <ndordoye@gmaail.com>
2026-05-10 05:27:21 +0000

    implemented the entire code in Main. Initially used functions to implement it.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,50 +7,36 @@ class Program
 
     static void Main()
     {
-        // Display a welcome message
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-
-        // Call funciton to start the guess game
-        GuessCompareRandom();
-    }
-
-    static int Generator()
-    {
-        // Store the random number generated in randonNumber variable
-        // Return the random number
+        // Generate a random number
         int randomNumber = RandomGenerator.Next(1, 101);
-        return randomNumber;
-    }
 
-    static void GuessCompareRandom()
-    {
-        // Call the random generator method and store the number in random variable
-        int random = Generator();
-
-        // number of guess
-        int guessCount = 0;
-
-        // declare a flag to reset the while loop 
-        string response = "yes";
-        while (response == "yes")
+        int guess = 0;    
+        int totalGuess = 0;
+        while(guess != randomNumber)
         {
-            Console.Write("Guess a magic number: ");
-            int guess = int.Parse(Console.ReadLine());  
-            guessCount++;
-
-            if(guess > random)
+            Console.Write("what is your magic number: ");
+            guess = int.Parse(Console.ReadLine());
+            totalGuess++;
+            
+            // compare the entered guess and give feedback to either guess
+            // higher or lower.
+            if(guess > randomNumber)
             {
-                Console.WriteLine("Lower!");
+                Console.WriteLine("Lower");
             }
-            else if(guess < random)
+            else if(guess < randomNumber)
             {
-                Console.WriteLine("Higher!");
+                Console.WriteLine("Higher");
             }
             else
             {
-                Console.WriteLine($"You guessed right in {guessCount} !");
-                response = "no";
+                Console.WriteLine("You guessed it!");
             }
         }
-    }   
+        // Display total number of guesses
+        Console.WriteLine($"Your total number of guess is {totalGuess}");
+
+
+    }
+
 }
