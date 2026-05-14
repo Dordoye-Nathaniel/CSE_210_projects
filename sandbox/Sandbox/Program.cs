3890fdea034078e7d6b1a709617b8d982d3d9dfe
Klenam <ndordoye@gmaail.com>
2026-05-14 21:58:25 +0000

    Created an instance of the Job and Resume class and displayed the attributes in myResume.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,67 @@
-using System;
 
-class Program
+
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Sandbox
 {
-    static void Main(string[] args)
+    public class Car
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        public string model;
+        public string brand;
+        public int year;
+
+        public void ShowCarDetails()
+        {
+           System.Console.WriteLine($"{brand} {model} {year}"); 
+        }
     }
+
+    public class Garage
+    {
+        public List<Car> Cars = new List<Car>(); 
+    }
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.Clear();
+            // var myCar = new Car();
+            // myCar.brand = "BMW";
+            // myCar.model = "CV7";
+            // myCar.year = 2026;
+
+            // myCar.ShowCarDetails();
+ 
+            var myGarage = new Garage();
+
+            while (true)
+            {
+                System.Console.WriteLine("Add cars to your garage (type done as brand to end)");
+                
+                Console.Write("Brand: ");
+                string brand = Console.ReadLine();
+                if (brand.ToLower() == "done")
+                {
+                    break;
+                }
+
+                Console.Write("Model: ");
+                string model = Console.ReadLine();
+
+                Console.Write("Year: ");
+                int year = int.Parse(Console.ReadLine());
+
+                myGarage.Cars.Add(new Car{brand = brand, model = model, year = year});
+            }
+            Console.Write("\nCars in your garage: ");
+            foreach (var car in myGarage.Cars)
+            {
+                car.ShowCarDetails();
+            }
+
+        }
+    }
+
 }
