b6586be27dff465e693fb4c42de53dcba4f447ce
Klenam <ndordoye@gmaail.com>
2026-05-27 16:55:51 +0000

    Displays methods from derived classes.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment("7.3", "8-19", "Roberto Rodriguez", "Fractions");
+        System.Console.WriteLine(math.GetSummary());
+        System.Console.WriteLine(math.MathHomeWorkList());
+
+        WritingAssignment writing = new WritingAssignment("The Causes of Word War II", "Mary Waters", "European History");
+        System.Console.WriteLine(writing.GetSummary());
+        System.Console.WriteLine(writing.GetWritingInformation());
     }
 }
