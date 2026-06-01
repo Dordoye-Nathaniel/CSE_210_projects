380e931e540dc4aabc8f95f1d8f4bd6537e787b1
Klenam <ndordoye@gmaail.com>
2026-06-01 19:45:52 +0000

    Calls methods and provides a menu to select each activity

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.Clear();
+
+
+
+        while (true)
+        {
+            System.Console.WriteLine("1. Start breathing activity.");
+            System.Console.WriteLine("2. Start reflecting activity.");
+            System.Console.WriteLine("3. Start listing activity.");
+            System.Console.WriteLine("4. Quit.");
+
+            System.Console.Write("Select a choice from the menu:");
+            string userEntry = Console.ReadLine()!;
+            if(userEntry == "4" || userEntry.ToLower() == "quit")
+            {
+                System.Console.WriteLine("Program ended!");
+                break;
+            }
+
+            else if(userEntry == "1")
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
+            }
+            else if (userEntry == "2")
+            {
+                ReflectingActivity reflectingActivity = new ReflectingActivity();
+                reflectingActivity.Run();
+            }
+            else if(userEntry == "3")
+            {
+                ListingActivity listingActivity = new ListingActivity();
+                listingActivity.Run();
+            }
+
+        }
+
     }
+
+
+
 }
