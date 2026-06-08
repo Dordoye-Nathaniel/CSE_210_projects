f960e432d5b82726cb3be160208fdcb22c829cd5
Klenam <ndordoye@gmaail.com>
2026-06-08 20:14:46 +0000

    Created a list of shapes and iterate over the list to print the color and area of the shape.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.Clear();
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square(5, "Blue"),
+            new Circle(2, "Green"),
+            new Rectangle(6, 5, "Cyan")
+        };
+
+        foreach(Shape shape in shapes)
+        {
+            System.Console.WriteLine($"Color: {shape.GetColor()}");
+            System.Console.WriteLine($"Area: {shape.GetArea()}");
+            System.Console.WriteLine();
+        }
     }
 }
