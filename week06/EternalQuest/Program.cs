8e2ccb7005bbf61c41246edbafcda42533305d9c
Klenam <ndordoye@gmaail.com>
2026-06-14 03:49:57 +0000

    Added a comment for exceeding requirement

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -2,6 +2,7 @@ class Program
 {
     static void Main()
     {
+        // Added ProgressGoals() as exceeding requirements 
         System.Console.WriteLine("Program starting....");
         GoalManager manager = new GoalManager();
         manager.Start();
