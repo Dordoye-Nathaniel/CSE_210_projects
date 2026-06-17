2b4f292705b5845f5e8fc9f4db3b236423ae9d87
Klenam <ndordoye@gmaail.com>
2026-06-17 19:15:43 +0000

    Added a swimming activity

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public int Laps
+    {
+        get { return _laps; }
+        set { _laps = value; }
+    }
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        Laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return Laps * 50.0 / 1000;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
+
+    public override string GetActivityName()
+    {
+        return "Swimming";
+    }
+}
